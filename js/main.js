4724efef7e87ae314b49e247c2adbda2eecdd60c
ASANCHEZWEB <desarrollosalbertosanchez@gmail.com>
2020-02-21 13:21:00 +0100

    las nubes ya se mueven

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log("comprobar main")
+//ANIMACIÓN DE NUBES
+//creo un setInterval que moverá las nubes cada x milisegundos
+setInterval(function() {
+  //Ahora busco en el DOM los hijos del contenedor de nubes (.nubes-container) para obtener las nubes en un array.
+  let nubesChilds = [...document.querySelectorAll(".nubes-container>img")];
+  //ahora recorremos cada imagen y le vamos restando la posicion left para que se muevan.
+  nubesChilds.forEach(function(element) {
+    //obtenemos la propiedad css ("left"), eliminamos el "px" y lo convertimos en numero
+    let elementLeftCss = Number(window.getComputedStyle(element).getPropertyValue("left").split("px")[0]);
+    //ahora cogemos el valor y le sumamos pixeles , las nubes ya se mueven .
+    element.style.left = `${elementLeftCss - 1}px`;
+  });
+}, 50);
