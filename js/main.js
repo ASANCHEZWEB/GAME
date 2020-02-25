4c3bd5748e9de767016f0a7f3b3e2ae49700a266
ASANCHEZWEB <desarrollosalbertosanchez@gmail.com>
2020-02-25 23:45:31 +0100

    componente de zorros.js simplificado

--- js/main.js
+++ js/main.js
@@ -6,13 +6,10 @@ setInterval(function() {
   if (marginLeft == -960) {
     nubesContainer.style.marginLeft = "0px";
   } else {
-    nubesContainer.style.marginLeft = `${marginLeft - 1}px`;
+    nubesContainer.style.marginLeft =`${marginLeft - 1}px`;
   }
 }, 30);
-//FUNCIONALIDAD ZORROS
-//Esta función genera 4 zorros al inicio del juego , toda la funcionalidad en "zorros.js"
+//GENERAR 4 ZORROS AL INICIO DEL JUEGO
 for (i = 0; i < 4; i++) {
-    let arraySizes = [100, 150, 200];
-    let randomSize = arraySizes[Math.floor(Math.random() * arraySizes.length)];
-    nuevoZorro(randomSize);
+    newZorro()
 }
--- js/zorros.js
+++ js/zorros.js
@@ -1,30 +1,27 @@
 let arrayZorros = [];
-
 let zorrosContainer = document.querySelector(".zorros-container");
-
 class Zorro {
-  constructor(width, marginLeft, marginTop, speed) {
+  constructor(width, height, marginLeft, marginTop) {
     this.width = width;
+    this.height = height;
     this.marginLeft = marginLeft;
     this.marginTop = marginTop;
-    this.speed = speed;
   }
-
   addToDOM() {
-    let newDivZorro = document.createElement("div");
-    newDivZorro.innerHTML = `<img src="./img/roca-redonda.png" alt="">`;
-    newDivZorro.style.width = `${this.width}px`;
-    newDivZorro.style.marginLeft = `${this.marginLeft}px`;
-    newDivZorro.style.marginTop = `${this.marginTop}px`;
-    zorrosContainer.appendChild(newDivZorro);
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML = `<img src="./img/roca-redonda.png" alt=""></img>`;
+    newDiv.style.width = `${this.width}px`;
+    newDiv.style.height = `${this.height}px`;
+    newDiv.style.marginLeft = `${this.marginLeft}px`;
+    newDiv.style.marginTop = `${this.marginTop}px`;
+    zorrosContainer.appendChild(newDiv);
   }
 }
-//FUNCIONES RELACIONADAS CON LOS ZORROS
-
-let nuevoZorro = function(size) {
-  let randomMarginLeft = Math.floor(Math.random() * 760);
-  let sizeZorro = size;
-  arrayZorros.push(new Zorro(sizeZorro, randomMarginLeft, 0, 0));
+//PRIMERO CREAMOS UN ZORRO
+let newZorro = function() {
+  let widthCases = [100, 150, 200];
+  let randomWidth = widthCases[Math.floor(Math.random() * widthCases.length)];
+  let randomMarginL = Math.floor(Math.random() * (960 - 0 + 1) + 0);
+  arrayZorros.push(new Zorro(randomWidth, 200, randomMarginL, 0));
   arrayZorros[arrayZorros.length - 1].addToDOM();
-  return;
 };
