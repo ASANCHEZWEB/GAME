74ca09554c10d8019342d659439264a7b6a7238c
ASANCHEZWEB <desarrollosalbertosanchez@gmail.com>
2020-02-28 00:54:38 +0100

    trabajando en método modifyMarginL

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 //ANIMACIÓN DE NUBES
 //creo un setInterval que moverá las nubes cada x milisegundos
 setInterval(function() {
-  let nubesContainer = document.querySelector(".nubes-container");
-  let marginLeft = Number(window.getComputedStyle(nubesContainer).getPropertyValue("margin-left").split("px")[0]);
-  if (marginLeft == -960) {
-    nubesContainer.style.marginLeft = "0px";
-  } else {
-    nubesContainer.style.marginLeft =`${marginLeft - 1}px`;
+    let nubesContainer = document.querySelector(".nubes-container");
+    let marginLeft = Number(window.getComputedStyle(nubesContainer).getPropertyValue("margin-left").split("px")[0]);
+    if (marginLeft == -770) {
+      nubesContainer.style.marginLeft = "0px";
+    } else {
+      nubesContainer.style.marginLeft =`${marginLeft - 1}px`;
+    }
+  }, 30);
+  //GENERAR 4 ZORROS AL INICIO DEL JUEGO
+  for (i = 0; i < 4; i++) {
+      newZorro();
   }
-}, 30);
-//GENERAR 4 ZORROS AL INICIO DEL JUEGO
-for (i = 0; i < 4; i++) {
-    newZorro();
-}
+  
--- js/zorros.js
+++ js/zorros.js
@@ -1,42 +1,43 @@
 let arrayZorros = [];
 let zorrosContainer = document.querySelector(".zorros-container");
-let widthContainer = Number(window.getComputedStyle(zorrosContainer).getPropertyValue("width").split("px")[0]);
-let heightContainer = Number(window.getComputedStyle(zorrosContainer).getPropertyValue("width").split("px")[0]);
 class Zorro {
-  constructor(width, height, marginLeft, marginTop) {
-    this.width = width;
-    this.height = height;
-    this.marginLeft = marginLeft;
-    this.marginTop = marginTop;
-  }
-  addToDOM() {
-    let newDiv = document.createElement("div");
-    newDiv.innerHTML = `<img src="./img/roca-redonda.png" alt=""></img>`;
-    newDiv.style.width = `${this.width}px`;
-    newDiv.style.height = `${this.height}px`;
-    newDiv.style.marginLeft = `${this.marginLeft}px`;
-    newDiv.style.marginTop = `${this.marginTop}px`;
-    zorrosContainer.appendChild(newDiv);
-  }
-  modifyMarginL(element, index) {
-    
-console.log(widthcontainer)
-  }
+    constructor(width, height, marginLeft, marginTop) {
+        this.width = width;
+        this.height = height;
+        this.marginLeft = marginLeft;
+        this.marginTop = marginTop;
+    }
+    addToDOM() {
+        let newDiv = document.createElement("div");
+        newDiv.innerHTML = `<img src="./img/roca-redonda.png" alt=""></img>`;
+        newDiv.style.width = `${this.width}px`;
+        newDiv.style.height = `${this.height}px`;
+        newDiv.style.marginLeft = `${this.marginLeft}px`;
+        newDiv.style.marginTop = `${this.marginTop}px`;
+        zorrosContainer.appendChild(newDiv);
+    }
+    modifyMarginL(element, index) {
+        this.marginLeft--;
+        element.modifyDOM(element, index);
+    }
+    modifyDOM(element, index) {
+        // ME HE QUEDADO POR ACTUALIZAR AQUI LA POSICION EN EL DOM
+    }
 }
 //FUNCIÓN CREA ZORRO
-let newZorro = function() {
-  let widthCases = [100, 150, 200];
-  let randomWidth = widthCases[Math.floor(Math.random() * widthCases.length)];
-  let randomMarginL = Math.floor(
-    Math.random() * (960 - randomWidth - 0 + 1) + 0
-  );
-  arrayZorros.push(new Zorro(randomWidth, 200, randomMarginL, 0));
-  arrayZorros[arrayZorros.length - 1].addToDOM();
+let newZorro = function () {
+    let widthCases = [80, 110, 130];
+    let randomWidth = widthCases[Math.floor(Math.random() * widthCases.length)];
+    let randomMarginL = Math.floor(
+        Math.random() * (770 - randomWidth - 0 + 1) + 0
+    );
+    arrayZorros.push(new Zorro(randomWidth, 130, randomMarginL, 0));
+    arrayZorros[arrayZorros.length - 1].addToDOM();
 };
 //FUNCIÓN MODIFICAR ZORROS
 let speedZorros = 30;
-setInterval(function() {
-  arrayZorros.forEach(function(element, index) {
-    element.modifyMarginL(element, index);
-  });
+setInterval(function () {
+    arrayZorros.forEach(function (element, index) {
+        element.modifyMarginL(element, index);
+    });
 }, speedZorros);
